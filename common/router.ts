5bb6aab76485163c6fbbb527d5a170af0055c051
Everton Gabriel Zoelner <everton.zoelner@gmail.com>
2018-10-26 01:00:29 -0300

    FIX Impedir de outra callback realizar tratativa após a execução do método render

--- common/router.ts
+++ common/router.ts
@@ -1,45 +1,42 @@
-import * as restify from 'restify'
-import {EventEmitter} from 'events'
-import {NotFoundError} from 'restify-errors'
+import * as restify from "restify";
+import { EventEmitter } from "events";
+import { NotFoundError } from "restify-errors";
 
 export abstract class Router extends EventEmitter {
-  abstract applyRoutes(application: restify.Server)
+  abstract applyRoutes(application: restify.Server);
 
   envelope(document: any): any {
-    return document
+    return document;
   }
 
   envelopeAll(documents: any[], options: any = {}): any {
-    return documents
+    return documents;
   }
 
-  render(response: restify.Response, next: restify.Next){
-    return (document)=>{
-      if(document){
-        this.emit('beforeRender', document)
-        response.json(this.envelope(document))
-      }else{
-        throw new NotFoundError('Documento não encontrado')
+  render(response: restify.Response, next: restify.Next) {
+    return document => {
+      if (document) {
+        this.emit("beforeRender", document);
+        response.json(this.envelope(document));
+      } else {
+        throw new NotFoundError("Documento não encontrado");
       }
-      return next()
-    }
+      return next(false);
+    };
   }
 
-  renderAll(response: restify.Response, next: restify.Next, options: any = {}){
-    return (documents: any[])=>{
-      if(documents){
-        documents.forEach((document, index, array)=>{
-          this.emit('beforeRender', document)
-          array[index] = this.envelope(document)
-        })
-        response.json(this.envelopeAll(documents, options))
-      }else{
-        response.json(this.envelopeAll([]))
+  renderAll(response: restify.Response, next: restify.Next, options: any = {}) {
+    return (documents: any[]) => {
+      if (documents) {
+        documents.forEach((document, index, array) => {
+          this.emit("beforeRender", document);
+          array[index] = this.envelope(document);
+        });
+        response.json(this.envelopeAll(documents, options));
+      } else {
+        response.json(this.envelopeAll([]));
       }
-      return next()
-    }
+      return next();
+    };
   }
-
-
-
 }
