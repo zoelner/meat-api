aeb16a35a7ed90c12c70a9ca2a6c8c39af09acac
Everton Gabriel Zoelner <everton.zoelner@gmail.com>
2018-10-26 01:03:09 -0300

    FIX Ajuste na mensagem de error 'ValidationError' para haver consistencia da requisição

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -1,28 +1,33 @@
-import * as restify from 'restify'
+import * as restify from "restify";
 
-export const handleError = (req: restify.Request, resp: restify.Response, err, done)=>{
-  
-  err.toJSON = ()=>{
+export const handleError = (
+  req: restify.Request,
+  resp: restify.Response,
+  err,
+  done
+) => {
+  err.toJSON = () => {
     return {
-      message : err.message
-    }
-  }
-  switch(err.name){
-    case 'MongoError':
-      if(err.code === 11000){
-        err.statusCode = 400
+      message: err.message
+    };
+  };
+  switch (err.name) {
+    case "MongoError":
+      if (err.code === 11000) {
+        err.statusCode = 400;
       }
-      break
-    case 'ValidationError':
-      err.statusCode = 400
-      const messages: any[] = []
-      for(let name in err.errors){
-        messages.push({message: err.errors[name].message})
+      break;
+    case "ValidationError":
+      err.statusCode = 400;
+      const messages: any[] = [];
+      for (let name in err.errors) {
+        messages.push({ message: err.errors[name].message });
       }
-      err.toJSON = ()=>({
+      err.toJSON = () => ({
+        message: "Validation error while processing your request",
         errors: messages
-      })
-      break
+      });
+      break;
   }
-  done()
-}
+  done();
+};
